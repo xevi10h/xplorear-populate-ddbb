d421a25867e827b22bde88635e7fd7ffa3a8f741
Xavier Huix Trenco <xeviht@MacBook-MacBook-Pro-de-Xavier.local>
2023-09-26 18:24:48 +0200

    update profile photo'

--- src/users/application/LoginGoogleUserUseCase.ts
+++ src/users/application/LoginGoogleUserUseCase.ts
@@ -1,6 +1,14 @@
 import jwt from "jsonwebtoken";
 import { MongoUserModel } from "../infrastructure/mongoModel/MongoUserModel.js";
 import IUser from "../domain/IUser.js";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import axios from "axios";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import sharp from "sharp";
 
 interface LoginGoogleUserDTO {
   email: string;
@@ -33,7 +41,6 @@ export default async function LoginGoogleUserUseCase({
       username,
       email,
       googleId,
-      photo,
       createdAt: new Date(),
       language: language || "en_US",
     });
@@ -46,5 +53,43 @@ export default async function LoginGoogleUserUseCase({
     }
   );
   user.token = token;
+
+  // If the user has already a photo, we don't need to upload it again
+  if (user.photo) {
+    const client = new S3Client({ region: "eu-west-1" });
+    const commandToGet = new GetObjectCommand({
+      Bucket: "xplorear-profile-img",
+      Key: user.id,
+    });
+    const url = await getSignedUrl(client, commandToGet, {
+      expiresIn: 3600 * 24,
+    }); // 1 day
+    user.photo = url;
+  } else if (photo) {
+    // If the user doesn't have a photo, we need to upload it
+    const image = await axios.get(photo, { responseType: "arraybuffer" });
+    const imageResized = await sharp(image.data)
+      .resize(250, 250, {
+        fit: "cover",
+      })
+      .toBuffer();
+    const client = new S3Client({ region: "eu-west-1" });
+    // Create a command to put a file into an S3 bucket.
+    const commandToPut = new PutObjectCommand({
+      Body: imageResized,
+      Bucket: "xplorear-profile-img",
+      Key: user.id,
+    });
+    await client.send(commandToPut);
+    // Create a command to get a file into an S3 bucket and then create a signed URL.
+    const commandToGet = new GetObjectCommand({
+      Bucket: "xplorear-profile-img",
+      Key: user.id,
+    });
+    const url = await getSignedUrl(client, commandToGet, {
+      expiresIn: 3600 * 24,
+    }); // 1 day
+    user.photo = url;
+  }
   return user.save();
 }
--- src/users/application/UpdateUserUseCase.ts
+++ src/users/application/UpdateUserUseCase.ts
@@ -1,5 +1,13 @@
 import { GraphQLError } from "graphql";
 import { MongoUserModel } from "../infrastructure/mongoModel/MongoUserModel.js";
+import axios from "axios";
+import sharp from "sharp";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 interface UpdateUserUseCaseDTO {
   tokenUserId: string;
@@ -10,7 +18,7 @@ interface UpdateUserUseCaseDTO {
   language?: string;
 }
 
-export default function UpdateUserUserCase({
+export default async function UpdateUserUserCase({
   tokenUserId,
   id,
   username,
@@ -26,12 +34,39 @@ export default function UpdateUserUserCase({
       },
     });
   }
+  let newPhoto = photo;
+  if (photo) {
+    // Extract binary from the base64 string.
+    const base64Data = photo.replace(/^data:image\/\w+;base64,/, "");
+    const dataBuffer = Buffer.from(base64Data, "base64");
+    const imageResized = await sharp(dataBuffer)
+      .resize(250, 250, {
+        fit: "cover",
+      })
+      .toBuffer();
+    const client = new S3Client({ region: "eu-west-1" });
+    // Create a command to put a file into an S3 bucket.
+    const commandToPut = new PutObjectCommand({
+      Body: imageResized,
+      Bucket: "xplorear-profile-img",
+      Key: id,
+    });
+    await client.send(commandToPut);
+    // Create a command to get a file into an S3 bucket and then create a signed URL.
+    const commandToGet = new GetObjectCommand({
+      Bucket: "xplorear-profile-img",
+      Key: id,
+    });
+    console.log("commandToGet", commandToGet);
+    const url = await getSignedUrl(client, commandToGet, { expiresIn: 3600 });
+    newPhoto = url;
+  }
   return MongoUserModel.findByIdAndUpdate(
     id,
     {
       username,
       name,
-      photo,
+      photo: newPhoto,
       language,
     },
     { new: true }
